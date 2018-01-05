979c72596893d14a96b427e951661dafee6ebd41
cachedview <35143646+cachedview@users.noreply.github.com>
2018-01-05 11:34:54 -0600

    files: Update website.

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,21 @@
 const root_site = "http://webcache.googleusercontent.com/search?q=cache:"
 var site;
 var keycode;
+var background;
 $(document).keyup(function(evt) {
   keycode = (evt.keyCode ? evt.keyCode : evt.which);
-  if (keycode == '13') {
-  site = root_site + $("#user_input").val()
-  window.open(site);
-}
+  if (keycode == '13') { // Enter
+    site = root_site + $("#user_input").val()
+    window.open(site);
+  }
+  if (!$("input").is(":focus")) {
+  if (keycode == '67') { // C
+    background = prompt("Change Background Color", "Enter RGB value, html color, or hex value");
+    $("body").css("background-color", background);
+  }
+  if (keycode == '73') { // I
+    background = prompt("Change Input Background Color", "Enter RGB value, html color, or hex value");
+    $("input").css("background-color", background);
+  }
+  }
 });
