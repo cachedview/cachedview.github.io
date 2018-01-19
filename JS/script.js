28f08895e10f19d0256837cdc811744b109529e0
cachedview <35143646+cachedview@users.noreply.github.com>
2018-01-18 19:55:34 -0600

    Update: Add download from url and improve layout.

--- JS/script.js
+++ JS/script.js
@@ -4,11 +4,15 @@ var keycode;
 var background;
 $(document).keyup(function(evt) {
   keycode = (evt.keyCode ? evt.keyCode : evt.which);
-  if (keycode == '13') { // Enter
+  if (!$("input").is(":focus")) {
+  if (keycode == '87') { // W
     site = root_site + $("#user_input").val()
     window.open(site);
   }
-  if (!$("input").is(":focus")) {
+  if (keycode == '85') { // W
+    $("#download_link").attr("href", $("#user_input").val());
+    $("#download_link").css("visibility", "visible");
+  }
   if (keycode == '67') { // C
     background = prompt("Change Background Color", "Enter RGB value, html color, or hex value");
     $("body").css("background-color", background);
